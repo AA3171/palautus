2221a0fce9d599a0bc6db7331621d6cbc9ac63bf
aa3171 <aa3171@student.jamk.fi>
2024-01-25 12:39:28 +0200

    2.16,2.17 tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import serverCom from './servercom'
+import './App.css'
 
 
 const Filter = (props) => {
@@ -9,7 +10,21 @@ const Filter = (props) => {
     <div>search <input value={props.search} onChange={props.handleSearch}/></div>
   )
 }
-
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  if (message === '404, person not found'){
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )}
+  return (
+    <div className="alert">
+      {message}
+    </div>)
+}
 
 
 const PersonForm = (props) => {
@@ -40,6 +55,7 @@ const App = () => {
   function deletefromregistry(victim){
     if (confirm("You sure?")){
     serverCom.killPerson(victim).then(response => {      console.log(response)    })}
+    setalertMessage("Deleted successfully")
   }
   function filterSearch(people){
     if (people.name.indexOf(search) != -1){
@@ -53,7 +69,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [search, setSearch] = useState('')
-
+  const [alertMessage, setalertMessage] = useState(null)
 
 
   const handleChangeNumber = (event) => {
@@ -66,7 +82,7 @@ const App = () => {
   const handleSearch = (event) => {
       setSearch(event.target.value)
     }
-
+    
     function containsObject(obj, list) {
       var i;
       for (i = 0; i < list.length; i++) {
@@ -91,26 +107,33 @@ const App = () => {
 
     if (containsObject(newName, persons)){
       if (confirm(newName + " is already in the registry. Would you like to change their number?")){
-        serverCom.replacenumber(returnObject(newName, persons), {name: newName, number: newNumber}).then(response => {      console.log(response)    })
+        serverCom.replacenumber(returnObject(newName, persons), {name: newName, number: newNumber})
+        .then(response => {      console.log(response)    })
+        .catch(error => {setalertMessage('404, person not found')})
+        setalertMessage(newName + " number changed successfully")
+        
       }
     }
     else {
       setPersons([...persons, {name: newName, number: newNumber}])
+      serverCom.create({name: newName, number: newNumber}).then(response => {      console.log(response)    })
+      setalertMessage(newName + " added successfully")
       setNewName("")
       setNumber("")  
-      serverCom.create({name: newName, number: newNumber}).then(response => {      console.log(response)    })
     }
   }
 
   serverCom.Update().then(response => {
     setPersons(response.data)})
 
+  if (alertMessage != null){
+    setTimeout(() => {setalertMessage(null)}, 1000)
+  }
 
   return (
     <div>
-      
-      
       <h2>Phonebook</h2>
+      <Notification message={alertMessage} />
       <Filter search={search} handleSearch={handleSearch}/>
       <h3>Add new</h3>
       <PersonForm addPerson={addPerson} newName={newName} handleNoteChange={handleNoteChange} 
