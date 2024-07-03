fe1d4ea4fadec20f59d0735fdcf1f7ecb681bc77
aa3171 <aa3171@student.jamk.fi>
2024-07-03 10:31:52 +0300

    3.1 tehty

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+let persons = [  
+    {    id: "1",    nimi: "Mika Häkkinen",    numero: 546756756  },  
+    {    id: "2",    nimi: "Koira",    numero: 56756756  },  
+    {    id: "3",    nimi: "Pankki automaatti",    numero: 345643  }]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
