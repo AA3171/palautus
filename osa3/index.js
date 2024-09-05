3e07f156ef69100fc9d4fa5e61c3423a5920412a
aa3171 <aa3171@student.jamk.fi>
2024-09-05 11:51:56 +0300

    3.9 korjaus

--- osa3/index.js
+++ osa3/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
+
+const cors = require('cors')
+
+
+
 const app = express()
 morgan.token('body', req => {
     return JSON.stringify(req.body)
@@ -7,11 +12,11 @@ morgan.token('body', req => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 let persons = [  
-    {    id: "1",    nimi: "Mika Häkkinen",    numero: 546756756  },  
-    {    id: "2",    nimi: "Koira",    numero: 56756756  },  
-    {    id: "3",    nimi: "Pankki automaatti",    numero: 345643  }]
+    {    id: "1",    name: "Mika Häkkinen",    number: 546756756  },  
+    {    id: "2",    name: "Koira",    number: 56756756  },  
+    {    id: "3",    name: "Pankki automaatti",    number: 345643  }]
 
-
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -51,13 +56,13 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
           
-    if (!body.nimi) {
+    if (!body.name) {
         return response.status(400).json({ 
         error: 'nimi puuttuu' 
         })
     }
     for (let i = 0; i < persons.length; i++) { 
-        if (body.nimi == persons[i]["nimi"]){
+        if (body.name == persons[i]["name"]){
             return response.status(400).json({
                 error: 'Nimi on jo luettelossa'
             })
@@ -65,7 +70,7 @@ app.post('/api/persons', (request, response) => {
     }
     
 
-    if (!body.numero) {
+    if (!body.number) {
         return response.status(400).json({ 
             error: 'numero puuttuu' 
         })
@@ -73,8 +78,8 @@ app.post('/api/persons', (request, response) => {
           
         const person = {
             id: generateId(),
-            nimi: body.nimi,
-            numero: body.numero,
+            name: body.name,
+            number: body.number,
         }
           
         persons = persons.concat(person)
